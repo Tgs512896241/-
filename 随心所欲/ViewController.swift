72b92cbf14eebfa1b63ef27ca356a6fc6979574e
Tgs <tgs@TgsdeMacBook-Pro.local>
2015-06-08 22:01:42 +0800

    Initial Commit

---
+++ 随心所欲/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  随心所欲
+//
+//  Created by Tgs on 15/6/8.
+//  Copyright (c) 2015年 Tgs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
